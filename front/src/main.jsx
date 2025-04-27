981fee09c9470c1b66f6c16780200cc6ca53b603
mgosalan-dev <mgosalan.dev@gmail.com>
2025-04-28 00:51:45 +0100

    #

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './pages/cadastro'
+import Home from './routes/AppRoutes'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
